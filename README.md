719965abae53cde8669670c70971edb6e43d1ab5
itslu666 <philipp.j6y@gmail.com>
2024-09-23 01:01:21 +0200

    added yay install func & added --skip-yay arg

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # autosetup
-Script to setup Linux stuff automatically
+Script to setup Linux stuff automatically.
+
+This script sets up:
+- git
+- yay
+- zsh + plugins
+- nano syntax highlighting
+- pacman color
+- starship zsh theme
+
+skip upgrade
+
+(This is my personal setup, feel free to use it though)
--- autosetup.sh
+++ autosetup.sh
@@ -28,8 +28,9 @@ else
     exit 0
 fi
 
+# git install func
 function install_git() {
-    echo "Installing git"
+    echo "Installing git..."
     case "${package_manager[$os]}" in
         pacman)
             sudo pacman -S --needed git
@@ -43,8 +44,45 @@ function install_git() {
     esac
 }
 
+# yay install func
 function install_yay() {
-    git clone
+    echo "installing yay..."
+    sudo pacman -S --needed base-devel
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si
+    cd ..
+    rm -rf yay
+    echo "Successfully installed yay"
+}
+
+# zsh instal func
+function install_zsh() {
+    if ! command -v curl &> /dev/null; then
+        if [[ -n "$(package_manager[$os])" ]]; then
+            case "${package_manager[$os]}" in
+                pacman)
+                    sudo pacman -S --needed curl
+                    ;;
+                apt)
+                    sudo apt update && sudo apt install -y curl
+                    ;;
+                dnf)
+                    sudo dnf install -y curl
+                    ;;
+            esac
+        else
+            echo "Curl is not installed and the package manager was not recognized. Install curl manually."
+            exit 0
+        fi
+    fi
+    
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 }
 
 if [ -z "$1" ]; then
+    # only install yay if arch os
+    if [[ "$os" == "arch" ]] && [[ ! " $@ " =~ " --skip-yay " ]]; then
+        install_git
+        install_yay
+    fi
